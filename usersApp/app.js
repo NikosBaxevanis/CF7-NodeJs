decbb59c2fffb409b37a1102a2d2ee2d44071d2d
NikosBaxevanis <baxemb@gmail.com>
2025-03-31 21:10:37 +0300

    adding products to users

--- usersApp/app.js
+++ usersApp/app.js
@@ -3,8 +3,11 @@ const app = express();
 
 app.use(express.json())
 const user = require('./roots/user.rootes');
+const userProduct = require('./roots/user.products.routes');
 
-app.use('/api/users', user)
+
+app.use('/api/users', user);
+app.use('/api/user-product', userProduct);
 
 module.exports = app 
 
