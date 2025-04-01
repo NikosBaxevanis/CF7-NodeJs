6400669b021bd93c48cda134685a0d7dbccefb7d
NikosBaxevanis <baxemb@gmail.com>
2025-04-01 21:04:11 +0300

    middlewares

--- usersApp/app.js
+++ usersApp/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json())
+
 const user = require('./roots/user.rootes');
 const userProduct = require('./roots/user.products.routes');
+const auth = require('./roots/auth.routes');
 
-
+app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
 
-module.exports = app 
+
+module.exports = app;
 
--- usersApp/roots/user.products.routes.js
+++ usersApp/roots/user.products.routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const userProductController = require('../controllers/user.product.controller');
 
 router.get('/', userProductController.findAll);
-//router.get('/:username', userProductController.findOne);
-router.post('/', userProductController.create)
+router.get('/:username', userProductController.findOne);
+router.post('/', userProductController.create); 
+router.patch('/:username', userProductController.update);
+router.delete('/:username/products/:id', userProductController.delete);
+router.get('/stats/stats1', userProductController.stats1);
 
 module.exports = router;
