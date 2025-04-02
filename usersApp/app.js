408172a72265a50f58ffcc67c7e17777040e985d
NikosBaxevanis <baxemb@gmail.com>
2025-04-02 21:04:25 +0300

    swagger

--- usersApp/app.js
+++ usersApp/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json())
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger')
+
 const user = require('./roots/user.rootes');
 const userProduct = require('./roots/user.products.routes');
 const auth = require('./roots/auth.routes');
@@ -11,6 +14,10 @@ app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
 
-
+app.use(
+    '/api-docs', 
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument.options)  
+  );
 module.exports = app;
 
--- usersApp/roots/user.rootes.js
+++ usersApp/roots/user.rootes.js
@@ -9,8 +9,8 @@ router.get('/', userController.findAll);
 router.get('/:username', userController.findOne);
 // router.post('/',userController.create);
 router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
-router.patch('/:username', userController.update);
-router.delete('/:username',userController.deleteByUsername);
-router.delete('/:username/email/:email',userController.deleteByEmail )
+router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update);
+router.delete('/:username',verifyToken, verifyRoles("ADMIN"),userController.deleteByUsername);
+router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail )
 
 module.exports = router;
