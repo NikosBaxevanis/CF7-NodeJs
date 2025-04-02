3ba96e759b9a2fce2271a73bea0f359d46c41c70
NikosBaxevanis <baxemb@gmail.com>
2025-04-02 20:12:03 +0300

    author admin

--- usersApp/controllers/auth.controller.js
+++ usersApp/controllers/auth.controller.js
@@ -1,7 +1,6 @@
-
 const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
-const authService = require('../services/auth.service')
+const authService = require('../services/auth.service');
 
 exports.login = async(req, res) =>{
   console.log("Login user", req.body);
@@ -10,12 +9,13 @@ exports.login = async(req, res) =>{
   const password = req.body.password;
   
   try {
-    const result = await User.findOne({username: username})
+    const result = await User.findOne({username: username},{username:1, email:1, password:1, roles:1})
+    console.log("User", result);
     const isMatch = await bcrypt.compare(password, result.password)
     
     // if (result && result.username === username && result.password === password){
     if (result && result.username === username && isMatch){
-        const token = authService.generateAccessToken(result)
+      const token = authService.generateAccessToken(result)
       res.status(200).json({status: true, data: token});
     } else {
       res.status(404).json({status: false, data: "user not logged in"});
