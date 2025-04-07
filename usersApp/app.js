841ad9a08bd278bb61ed21acf67154a0bd46bdf6
NikosBaxevanis <baxemb@gmail.com>
2025-04-07 21:04:53 +0300

    swagger

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const app = express();
+const app = express()
 
-app.use(express.json())
+app.use(express.json());
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger')
+const swaggerDocument = require('./swagger');
 
-const user = require('./roots/user.rootes');
-const userProduct = require('./roots/user.products.routes');
-const auth = require('./roots/auth.routes');
+const user = require('./routes/user.routes');
+const userProduct = require('./routes/user.products.routes')
+const auth = require('./routes/auth.routes');
 
 app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
 
 app.use(
-    '/api-docs', 
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument.options)  
-  );
-module.exports = app;
+  '/api-docs', 
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument.options)  
+);
 
+module.exports = app
--- usersApp/services/user.services.js
+++ usersApp/services/user.services.js
@@ -1,14 +1,13 @@
-const User = require('../models/user.model')
+const User = require('../models/user.model');
 
-function findAll () {
-    const result = User.find();
-    return result;
+function findAll() {
+  const result = User.find();
+  return result;
 }
 
-function findOne (username) {
-    const result = User.findOne({username:username});
-    return result;
-
+function findOne(username) {
+  const result = User.findOne({username:username});
+  return result;
 }
 
-module.exports = {findAll , findOne} 
+module.exports = { findAll, findOne }
